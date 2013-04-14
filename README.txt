54f41779fc5e320f9f4d14daa5d01b45cfd6b9e0
yidao620c <yidao620@gmail.com>
2013-04-14 20:37:40 +0800

    first commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+java并发编程source
---
+++ src/main/java/net/jcip/examples/LazyInitRace.java
@@ -0,0 +1,26 @@
+package net.jcip.examples;
+
+import net.jcip.annotations.*;
+
+/**
+ * LazyInitRace
+ * <p/>
+ * Race condition in lazy initialization
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+
+@NotThreadSafe
+public class LazyInitRace {
+    private ExpensiveObject instance = null;
+
+    public ExpensiveObject getInstance() {
+        if (instance == null)
+            instance = new ExpensiveObject();
+        return instance;
+    }
+}
+
+class ExpensiveObject {
+}
+
